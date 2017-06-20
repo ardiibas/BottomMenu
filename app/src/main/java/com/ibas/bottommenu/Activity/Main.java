4dff2edcd787386cd5a780382065f5304f487b10
Ardi Baskoro <mistersemar.ab@gmail.com>
2017-06-20 19:10:16 +0700

    Initial commit

---
+++ app/src/main/java/com/ibas/bottommenu/Activity/Main.java
@@ -0,0 +1,20 @@
+package com.ibas.bottommenu.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ibas.bottommenu.R;
+
+/**
+ * Created by Ibas on 20/06/2017.
+ */
+
+public class Main extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
---
+++ app/src/main/java/com/ibas/bottommenu/Fragment/Fragment.java
@@ -0,0 +1,8 @@
+package com.ibas.bottommenu.Fragment;
+
+/**
+ * Created by Ibas on 20/06/2017.
+ */
+
+public class Fragment {
+}
